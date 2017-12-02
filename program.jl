cef3ed07100d78fdd003df9c08ee64a0e2c1ff46
tao24 <32672337+tao24@users.noreply.github.com>
2017-12-02 13:03:56 +0100

    metoda newtona

--- program.jl
+++ program.jl
@@ -1,13 +1,21 @@
 setprecision(32)
 
-data = Array{BigFloat, 2}( [234 543 213 4732; 132 756 326 5648; 321 564 435 3543; 46832 63434 42683 83429])
+data = Array{BigFloat, 2}( [
+    234 231 321 213; 
+    123 234 352 302; 
+    342 413 643 521; 
+    354 432 642 523])
+
+@show data[1, :]
 
 # data[1, :] : wektor współrzędnych x satelit
 # data[2, :] : wektor współrzędnych y satelit
 # data[3, :] : wektor współrzędnych z satelit
 # data[4, :] : wektor czasów zegara satelit
 
-C = 3000000
+# data[:, i] : dane z i-tej satelity
+
+C = 1 # dla testów
 
 function f(i::Int64, X::Array{BigFloat})
     return (x[1] - data[1, i]) * (x[1] - data[1, i]) + 
@@ -20,14 +28,35 @@ function F(X::Array{BigFloat})
     return [f(j, X) for j = 1:4]
 end
 
-Jacobian = Array{Function, 2}(4, 4)
+JacobianArray = Array{Function, 2}(4, 4)
 
 for j = 1:3
-    for i = 1:4 Jacobian[j, i] = function(x) return 2*x[j] - 2*data[j, i] end end
+    for i = 1:4 JacobianArray[j, i] = function(x) return 2*x[j] - 2*data[j, i] end end
 end
-for i = 1:4 Jacobian[4, i] = function(x) return 2*C*data[4, i] - 2*C*x[4] end end
+for i = 1:4 JacobianArray[4, i] = function(x) return 2*C*data[4, i] - 2*C*x[4] end end
+
+function Jacobian(JacobianArray::Array{Function, 2}, X::Array{BigFloat})
+    sol = Array{BigFloat, 2}(4, 4)
+    for i = 1:4
+        for j = 1:4
+            sol[i, j] = JacobianArray[i, j](X)
+        end
+    end
+    return sol
+end
+
+function NewtonMethod(X::Array{BigFloat}, ϵ::BigFloat)
+    iteration_count = 1
+    δ = - inv(Jacobian(JacobianArray, X)) * F(X)
+    while norm(δ) > ϵ        
+        X = X + δ
+        δ = - inv(Jacobian(JacobianArray, X)) * F(X)
+        @show X
+    end
+    return X + δ
+end
 
 x = Array{BigFloat, 1}(3)
 x = Array{BigFloat, 1}([1, 2, 3, 4])
 
-@show Jacobian[1, 1](234)
+@show NewtonMethod(Array{BigFloat, 1}([1, 1, 1, 1]), BigFloat(1));
