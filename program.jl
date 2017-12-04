5eec9312f31a83c779e346307aa1c9f7188bcb0d
tao24 <32672337+tao24@users.noreply.github.com>
2017-12-04 19:28:33 +0100

    Newton's method
    Dodałem metodę newtona do notebooka i pliku programu, przykładowy test i jedno z rozwiązań

--- program.jl
+++ program.jl
@@ -1,4 +1,4 @@
-setprecision(32)
+setprecision(64)
 
 data = Array{BigFloat, 2}( [
     234 231 321 213; 
@@ -17,23 +17,23 @@ data = Array{BigFloat, 2}( [
 
 C = 1 # dla testów
 
-function f(i::Int64, X::Array{BigFloat})
-    return (x[1] - data[1, i]) * (x[1] - data[1, i]) + 
-            (x[2] - data[2, i]) * (x[2] - data[2, i]) +
-            (x[3] - data[3, i]) * (x[3] - data[3, i]) - 
-            C * (data[4, i] - x[4]) * (data[4, i] - x[4])
-end
+solution₁ = Array{BigFloat}([BigFloat("3.406027414052647875887849459335517363700416540639706155009830367636869268148760385343e+02"),
+                            BigFloat("2.245621396967134977305526534214474645554267237451765234641071107647602855199079076453e+02"),
+                            BigFloat("8.802209804658630277006504596604842085109012249373217265353711792757437615214584985008e+02"),
+                            BigFloat("9.119971653230215237445022093317229764207208762060580107951832552971171535934449105973e+02")])
 
-function F(X::Array{BigFloat})
-    return [f(j, X) for j = 1:4]
+function f(x::Array{BigFloat})
+    return ((-data .+ x) .^ 2)' * [1, 1, 1, -C]
 end
 
 JacobianArray = Array{Function, 2}(4, 4)
 
-for j = 1:3
-    for i = 1:4 JacobianArray[j, i] = function(x) return 2*x[j] - 2*data[j, i] end end
+for i = 1:4
+    for j = 1:3
+        JacobianArray[i, j] = function(x) return 2*x[j] - 2*data[j, i] end
+    end
+    JacobianArray[i, 4] = function(x) return 2*C*data[4, i] - 2*C*x[4] end
 end
-for i = 1:4 JacobianArray[4, i] = function(x) return 2*C*data[4, i] - 2*C*x[4] end end
 
 function Jacobian(JacobianArray::Array{Function, 2}, X::Array{BigFloat})
     sol = Array{BigFloat, 2}(4, 4)
@@ -47,16 +47,17 @@ end
 
 function NewtonMethod(X::Array{BigFloat}, ϵ::BigFloat)
     iteration_count = 1
-    δ = - inv(Jacobian(JacobianArray, X)) * F(X)
-    while norm(δ) > ϵ        
-        X = X + δ
-        δ = - inv(Jacobian(JacobianArray, X)) * F(X)
-        @show X
+    while true 
+        @show X - solution₁
+        δ = Jacobian(JacobianArray, X) \ f(X)
+        X = X - δ
+        if norm(δ) < ϵ
+            return x
+        end
     end
-    return X + δ
 end
 
 x = Array{BigFloat, 1}(3)
 x = Array{BigFloat, 1}([1, 2, 3, 4])
-
-@show NewtonMethod(Array{BigFloat, 1}([1, 1, 1, 1]), BigFloat(1));
+# [223.905, 61.0486, 413.446, 258.905]
+@show NewtonMethod(Array{BigFloat, 1}([1, 1, 1, 0]), BigFloat("0.0000000000001"));
